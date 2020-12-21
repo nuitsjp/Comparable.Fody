1eefa20d8daf6867559f2590d598ba09323bf988
Atsushi Nakamura <nuits.jp@live.jp>
2020-12-22 05:21:08 +0900

    Add Struct test case.

---
+++ Source/AssemblyToProcess/StructWithDoubleValue.cs
@@ -0,0 +1,24 @@
+﻿using Comparable;
+// ReSharper disable UnusedMember.Global
+
+namespace AssemblyToProcess
+{
+    [Comparable]
+    public struct StructWithDoubleValue
+    {
+        [CompareBy(Priority = 2)]
+        private string _value1;
+
+        [CompareBy]
+        public int Value0 { get; set; }
+
+        // ReSharper disable once ConvertToAutoProperty
+        public string Value1
+        {
+            get => _value1;
+            set => _value1 = value;
+        }
+
+        public int NotCompareValue { get; set; }
+    }
+}
--- Source/Comparable.Fody.Test/ImplementTestForStruct.cs
+++ Source/Comparable.Fody.Test/ImplementTestForStruct.cs
@@ -16,6 +16,38 @@ namespace Comparable.Fody.Test
             }
 
             [Fact]
+            public void Return1WhenComparedToNull()
+            {
+                var comparable = (IComparable)TestResult.GetInstance("AssemblyToProcess.StructWithSingleProperty");
+                comparable.CompareTo(null)
+                    .Should().Be(1);
+            }
+
+            [Fact]
+            public void ThrowArgumentExceptionWhenComparedToDifferentType()
+            {
+                var comparable = (IComparable)TestResult.GetInstance("AssemblyToProcess.StructWithSingleProperty");
+                var instanceOfDifferentType = string.Empty;
+                comparable.Invoking(x => x.CompareTo(instanceOfDifferentType))
+                    .Should().Throw<ArgumentException>()
+                    .WithMessage("Object is not a AssemblyToProcess.StructWithSingleProperty.");
+            }
+
+            [Fact]
+            public void ReturnCompareToResultOfSingleProperty()
+            {
+                var instance0 = TestResult.GetInstance("AssemblyToProcess.StructWithSingleProperty");
+                instance0.Value = 1;
+                var instance1 = TestResult.GetInstance("AssemblyToProcess.StructWithSingleProperty");
+                instance1.Value = 2;
+
+                ((IComparable)instance0).CompareTo((object)instance1)
+                    .Should().Be(instance0.Value.CompareTo(instance1.Value));
+
+            }
+
+
+            [Fact]
             public void ReturnCompareToResultOfSingleField()
             {
                 var instance0 = TestResult.GetInstance("AssemblyToProcess.StructWithSingleField");
@@ -25,6 +57,21 @@ namespace Comparable.Fody.Test
 
                 ((IComparable)instance0).CompareTo((object)instance1)
                     .Should().Be(instance0.Value.CompareTo(instance1.Value));
+
+            }
+
+            [Fact]
+            public void ReturnCompareToResultOfDoubleValue()
+            {
+                var instance0 = TestResult.GetInstance("AssemblyToProcess.StructWithDoubleValue");
+                instance0.Value0 = 1;
+                instance0.Value1 = "1";
+                var instance1 = TestResult.GetInstance("AssemblyToProcess.StructWithDoubleValue");
+                instance1.Value0 = 2;
+                instance1.Value1 = "2";
+
+                ((IComparable)instance0).CompareTo((object)instance1)
+                    .Should().Be(instance0.Value1.CompareTo(instance1.Value1));
 
             }
         }
