6d17ce26b556d84d04d8b7ef74a601deae7683a2
Atsushi Nakamura <nuits.jp@live.jp>
2021-01-02 05:37:12 +0900

    Refactor for CompareByFieldDefinition.

--- Source/Comparable.Fody/CompareByFieldDefinition.cs
+++ Source/Comparable.Fody/CompareByFieldDefinition.cs
@@ -8,25 +8,26 @@ namespace Comparable.Fody
 {
     public class CompareByFieldDefinition : CompareByMemberDefinitionBase
     {
+
+        private readonly FieldDefinition _fieldDefinition;
+
         public CompareByFieldDefinition(IComparableModuleDefine comparableModuleDefine, FieldDefinition fieldDefinition)
             : base(comparableModuleDefine, fieldDefinition, fieldDefinition.FieldType)
         {
-            FieldDefinition = fieldDefinition;
+            _fieldDefinition = fieldDefinition;
         }
-
-        private FieldDefinition FieldDefinition { get; }
 
         public override void AppendCompareTo(ILProcessor ilProcessor, ParameterDefinition parameterDefinition)
         {
             ilProcessor.Append(Instruction.Create(OpCodes.Ldarg_0));
-            ilProcessor.Append(Instruction.Create(OpCodes.Ldfld, FieldDefinition));
+            ilProcessor.Append(Instruction.Create(OpCodes.Ldfld, _fieldDefinition));
             if (MemberTypeDefinition.IsStruct)
             {
                 ilProcessor.Append(Instruction.Create(OpCodes.Stloc_S, LocalVariable));
                 ilProcessor.Append(Instruction.Create(OpCodes.Ldloca_S, LocalVariable));
             }
             ilProcessor.Append(Instruction.Create(OpCodes.Ldarg_1));
-            ilProcessor.Append(Instruction.Create(OpCodes.Ldfld, FieldDefinition));
+            ilProcessor.Append(Instruction.Create(OpCodes.Ldfld, _fieldDefinition));
             ilProcessor.Append(MemberTypeDefinition.IsStruct
                 ? Instruction.Create(OpCodes.Call, CompareToMethodReference)
                 : Instruction.Create(OpCodes.Callvirt, CompareToMethodReference));
