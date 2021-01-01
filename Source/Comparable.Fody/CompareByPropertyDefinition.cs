f14dcd3dfafde2b94488fe6135113ab7c43e62a2
Atsushi Nakamura <nuits.jp@live.jp>
2021-01-02 05:59:06 +0900

    Refactor for CompareByPropertyDefinition.

--- Source/Comparable.Fody/CompareByPropertyDefinition.cs
+++ Source/Comparable.Fody/CompareByPropertyDefinition.cs
@@ -9,33 +9,33 @@ namespace Comparable.Fody
     public class CompareByPropertyDefinition : CompareByMemberDefinitionBase
     {
 
-        private readonly PropertyDefinition _propertyDefinition;
+        private readonly PropertyDefinition _memberDefinition;
 
-        public CompareByPropertyDefinition(IComparableModuleDefine comparableModuleDefine, PropertyDefinition propertyDefinition)
-            : base(comparableModuleDefine, propertyDefinition, propertyDefinition.PropertyType)
+        public CompareByPropertyDefinition(IComparableModuleDefine comparableModuleDefine, PropertyDefinition memberDefinition)
+            : base(comparableModuleDefine, memberDefinition, memberDefinition.PropertyType)
         {
-            _propertyDefinition = propertyDefinition;
+            _memberDefinition = memberDefinition;
         }
 
         public override void AppendCompareTo(ILProcessor ilProcessor, ParameterDefinition parameterDefinition)
         {
             ilProcessor.Append(Instruction.Create(OpCodes.Ldarg_0));
-            ilProcessor.Append(Instruction.Create(OpCodes.Call, _propertyDefinition.GetMethod));
+            ilProcessor.Append(Instruction.Create(OpCodes.Call, _memberDefinition.GetMethod));
             if (MemberTypeDefinition.IsStruct)
             {
                 ilProcessor.Append(Instruction.Create(OpCodes.Stloc_S, LocalVariable));
                 ilProcessor.Append(Instruction.Create(OpCodes.Ldloca_S, LocalVariable));
             }
 
-            if (_propertyDefinition.DeclaringType.IsStruct())
+            if (_memberDefinition.DeclaringType.IsStruct())
             {
                 ilProcessor.Append(Instruction.Create(OpCodes.Ldarga_S, parameterDefinition));
-                ilProcessor.Append(Instruction.Create(OpCodes.Call, _propertyDefinition.GetMethod));
+                ilProcessor.Append(Instruction.Create(OpCodes.Call, _memberDefinition.GetMethod));
             }
             else
             {
                 ilProcessor.Append(Instruction.Create(OpCodes.Ldarg_1));
-                ilProcessor.Append(Instruction.Create(OpCodes.Callvirt, _propertyDefinition.GetMethod));
+                ilProcessor.Append(Instruction.Create(OpCodes.Callvirt, _memberDefinition.GetMethod));
             }
 
             ilProcessor.Append(MemberTypeDefinition.IsStruct
