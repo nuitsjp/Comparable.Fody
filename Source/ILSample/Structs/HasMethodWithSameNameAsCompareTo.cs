433ddedd2700b72b0f137f3640fff21c2b2151f3
Atsushi Nakamura <nuits.jp@live.jp>
2021-01-01 10:54:37 +0900

    Add HasMethodWithSameNameAsCompareTo.

---
+++ Source/ILSample/Structs/HasMethodWithSameNameAsCompareTo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ILSample.Structs
+{
+    public struct HasMethodWithSameNameAsCompareTo : IComparable
+    {
+        private readonly string _value;
+
+        public HasMethodWithSameNameAsCompareTo(string value)
+        {
+            _value = value;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return int.MaxValue;
+        }
+
+
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj is null) return 1;
+
+            if (!(obj is HasMethodWithSameNameAsCompareTo))
+            {
+                throw new ArgumentException("Object is not a HasMethodWithSameNameAsCompareTo");
+            }
+            return CompareTo((HasMethodWithSameNameAsCompareTo)obj);
+        }
+
+        public int CompareTo(HasMethodWithSameNameAsCompareTo obj)
+        {
+            if (obj is null) return 1;
+
+            return _value.CompareTo(obj._value);
+        }
+    }
+}
