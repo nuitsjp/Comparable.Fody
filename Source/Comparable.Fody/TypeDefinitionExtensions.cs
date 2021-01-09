506e56cf918b09bc08a25b67603f82510934811c
Atsushi Nakamura <nuits.jp@live.jp>
2021-01-10 08:48:21 +0900

    Translate comments into English.

--- Source/Comparable.Fody/TypeDefinitionExtensions.cs
+++ Source/Comparable.Fody/TypeDefinitionExtensions.cs
@@ -68,36 +68,27 @@ namespace Comparable.Fody
         }
 
         /// <summary>
-        /// CompareToメソッドを取得する。
+        /// Get the CompareTo method.
         /// </summary>
         /// <remarks>
-        /// typeDefinitionに定義されているCompareToメソッドのリファレンスを取得する。
-        /// この時、Genericの実装と、非Genericの実装が混在する場合がある。
-        /// 一般的にGenericの実装が処理効率が良いため、実装クラスと引数にとるCompareToを優先する。
+        /// Get the reference of the CompareTo method defined in typeDefinition.
+        /// The typeDefinition may be a mixture of generic and non-generic implementations.
+        /// In general, generic implementations are more efficient, so priority is given to the implementation class and the CompareTo argument.
         /// 
-        /// また、引数のクラスには直接定義されておらず、基底クラスなどで定義されている可能性がある。
+        /// Also, it is not directly defined in the argument class, but may be defined in the base class.
         /// 
-        /// 引数がクラスの場合。
-        /// 再帰的に基底クラスを探索する。
+        /// If typeDefinition is a class.
+        /// Recursively search for a base class.
         /// 
-        /// 引数がインターフェースの場合、クラスと異なり多重実装している可能性に注意する。
-        /// 異なるGenericなIComparableの実装を多重実装が存在した場合、どちらを優先すべきか決定できない。
-        /// そのため、GenericなIComparableが複数実装されていた場合、非Genericな実装を優先する。
+        /// If the argument is an interface, be aware of the possibility of multiple implementations.
+        /// If there are multiple implementations of different generic IComparable implementations, it is not possible to decide which one should be preferred.
+        /// Therefore, if more than one generic IComparable is implemented, priority is given to the non-generic implementation.
         /// </remarks>
         /// <param name="typeDefinition"></param>
         /// <returns></returns>
         internal static MethodReference GetCompareToMethodReference(this TypeDefinition typeDefinition)
         {
-            // If "typeDefinition" is IComparable or Generic IComparable
-            if (typeDefinition.IsInterface
-                // ReSharper disable once AssignNullToNotNullAttribute
-                && typeDefinition.FullName.StartsWith(typeof(IComparable).FullName))
-            {
-                return typeDefinition.Methods
-                    .Single(x => x.Name == nameof(IComparable.CompareTo));
-            }
-
-            // Genericの実装が処理効率が良いため、実装クラスを引数にとるCompareToを取得する。
+            // Since the Generic implementation is more efficient to process, we will get a CompareTo that takes the implementation class as an argument.
             MethodReference compareTo = typeDefinition.Methods
                 .SingleOrDefault(methodDefinition =>
                     methodDefinition.Name == nameof(IComparable.CompareTo)
@@ -105,7 +96,7 @@ namespace Comparable.Fody
                     && methodDefinition.Parameters.Single().ParameterType.FullName == typeDefinition.FullName);
             if (compareTo is not null) return compareTo;
 
-            // Genericの実装がない場合、非Genericな実装を取得する
+            // If there is no Generic implementation, get a non-Generic implementation.
             compareTo = typeDefinition.Methods
                 .SingleOrDefault(methodDefinition =>
                     methodDefinition.Name == nameof(IComparable.CompareTo)
@@ -113,53 +104,53 @@ namespace Comparable.Fody
                     && methodDefinition.Parameters.Single().ParameterType.FullName == typeof(Object).FullName);
             if (compareTo is not null) return compareTo;
 
-            // 非Genericな実装も存在しない場合
+            // If the implementation does not exist
             if (typeDefinition.IsInterface)
             {
-                // GenericなIComparableを取得する
-                // ReSharper disable once IdentifierTypo
-                var genericIComparables = typeDefinition
+                // For interface
+                var comparables = typeDefinition
                     .Interfaces
                     .Select(x => x.InterfaceType)
-                    .Where(x => x.IsGenericInstance)
                     .Where(x => x.FullName.StartsWith(typeof(IComparable).FullName!))
                     .ToList();
-                if (genericIComparables.Count == 1)
+
+                if (comparables.Empty())
                 {
-                    var genericIComparable = (GenericInstanceType)genericIComparables.Single();
+                    // If IComparable is not implemented, it will recursively search for the parent.
+                    foreach (var interfaceReference in typeDefinition.Interfaces.Select(x => x.InterfaceType))
+                    {
+                        compareTo = interfaceReference.Resolve().GetCompareToMethodReference();
+                        if (compareTo is not null) return compareTo;
+                    }
+
+                    // When searching for an Interface recursively, if the destination does not implement IComparable, null is returned.
+                    return null;
+                }
+
+                // Getting a Generic IComparable
+                var genericComparables = comparables
+                    .Where(x => x.IsGenericInstance)
+                    .Cast<GenericInstanceType>()
+                    .ToArray();
+                if (genericComparables.Count() == 1)
+                {
+                    var genericIComparable = genericComparables.Single();
                     var genericCompareTo = genericIComparable.Resolve()
                         .Methods
                         .Single(x => x.Name == nameof(IComparable.CompareTo));
-                    genericIComparable.Module.ImportReference(genericCompareTo);
                     return genericCompareTo.MakeGeneric(genericIComparable.GenericArguments.ToArray());
                 }
 
-                // GenericなIComparableが複数ある場合、非GenericなIComparableから取得する
-                var nonGenericIComparable = typeDefinition
-                    .Interfaces
-                    .Select(x => x.InterfaceType)
-                    .Where(x => !x.IsGenericInstance)
-                    .SingleOrDefault(x => x.FullName == typeof(IComparable).FullName);
-                if (nonGenericIComparable is not null)
-                {
-                    return nonGenericIComparable.Resolve().GetCompareToMethodReference();
-                }
-
-                // IComparableを直接実装していない場合、再帰的に親を探索する。
-                foreach (var interfaceReference in typeDefinition.Interfaces.Select(x => x.InterfaceType))
-                {
-                    compareTo = interfaceReference.Resolve().GetCompareToMethodReference();
-                    if (compareTo is not null) return compareTo;
-                }
-            }
-            else
-            {
-                // typeDefinitionがインターフェースではない場合、Baseクラスを再帰的に探索する
-                return typeDefinition.BaseType.Resolve().GetCompareToMethodReference();
+                // If there are multiple generic IComparables, get them from non-generic IComparables.
+                return comparables
+                    .SingleOrDefault(x => !x.IsGenericInstance)
+                    ?.Resolve()
+                    .GetCompareToMethodReference();
             }
 
-            // Interfaceを再帰的に探索している場合、探索先がIComparableを実装していなかった場合はnullを返す。
-            return null;
+            // For class.
+            // If typeDefinition is not an interface, recursively search the Base class
+            return typeDefinition.BaseType.Resolve().GetCompareToMethodReference();
         }
 
         internal static TypeReference GetGenericTypeReference(this TypeReference typeReference)
